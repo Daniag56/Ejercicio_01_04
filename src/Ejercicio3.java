38e7b73905241dc78ecef40271192329a5c2051a
Daniel Arias <dnlrsgrc@gmail.com>
2025-09-24 19:24:55 +0200

    feat: Ejercicio4 Tema1

---
+++ src/Ejercicio3.java
@@ -0,0 +1,19 @@
+public class Ejercicio3 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("LUNES  MARTES  MIERCOLES  JUEVES  VIERNES");
+        System.out.printf("""
+                                 PROG                       BD\r
+                                 PROG                       BD\r
+                                 PROG                PROG         \r
+                                 BD                  PROG         \r
+                          BD     BD       PROG       PROG          \r
+                          BD              PROG                 \
+                          """ //
+        //
+        //
+        //
+        //
+        //
+        );
+    }
+}

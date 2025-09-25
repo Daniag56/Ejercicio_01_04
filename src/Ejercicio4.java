b1d1801385e60c0f6eece73531dbea3cc6dccc49
Daniel Arias <dnlrsgrc@gmail.com>
2025-09-25 19:09:57 +0200

    feat: Horario

---
+++ src/Ejercicio4.java
@@ -0,0 +1,13 @@
+public class Ejercicio4 {
+    public static void main(String[] args) throws Exception {
+        System.out.println();
+        System.out.println("LUNES  MARTES  MIERCOLES  JUEVES  VIERNES");
+        System.out.printf("        %10s                        %10s\n",Utils.GREEN+"PROG"+Utils.RESET,Utils.BLUE_BOLD+"BD"+Utils.RESET);
+        System.out.printf("        %10s                        %10s\n",Utils.GREEN+"PROG"+Utils.RESET,Utils.BLUE_BOLD+"BD"+Utils.RESET);
+        System.out.printf("        %10s               %10s\n",Utils.GREEN+"PROG"+Utils.RESET,Utils.GREEN+"PROG"+Utils.RESET);
+        System.out.printf("        %10s                 %10s\n",Utils.BLUE_BOLD+"BD"+Utils.RESET,Utils.GREEN+"PROG"+Utils.RESET);
+        System.out.printf("%10s      %10s      %10s       %10s\n",Utils.BLUE_BOLD+"BD"+Utils.RESET,Utils.BLUE_BOLD+"BD"+Utils.RESET,Utils.GREEN+"PROG"+Utils.RESET,Utils.GREEN+"PROG"+Utils.RESET);
+        System.out.printf("%10s              %10s\n",Utils.BLUE_BOLD+"BD"+Utils.RESET,Utils.GREEN+"PROG"+Utils.RESET);
+       
+    }
+}
